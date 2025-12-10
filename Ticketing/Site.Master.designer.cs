225474dd13d9d8e05a4c9d990746ac769cbc09f8
Angelo-Rend <angelo.rendinefg@gmail.com>
2025-12-10 12:25:46 +0100

    Revert "merge"
    This reverts commit 86c2c525532cfddabb75e5a9e4d174f7276e01cd, reversing
    changes made to 40ee12d59f9721efec135928f11b6f798a0baaf7.

--- Ticketing/Site.Master.designer.cs
+++ Ticketing/Site.Master.designer.cs
@@ -15,15 +15,6 @@ namespace Ticketing
     {
 
         /// <summary>
-        /// Controllo LogoNotifica.
-        /// </summary>
-        /// <remarks>
-        /// Campo generato automaticamente.
-        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image LogoNotifica;
-
-        /// <summary>
         /// Controllo MainContent.
         /// </summary>
         /// <remarks>
